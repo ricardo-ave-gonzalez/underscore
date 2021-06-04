54a7716edc37ffbe4d08833deb4b48eb558c83d6
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2021-06-04 12:41:08 -0300

    size 202104061241

---
+++ size.js
@@ -0,0 +1,36 @@
+const { _ } = require('underscore')
+/**
+ *      Return the number of values in the list.
+ *      _.size([1, 2, 3, 4, 5]);
+ *      => 5
+ *      _.size({one: 1, two: 2, three: 3});
+ *      => 3
+ */
+
+let goal = [
+    {
+        "category" : "other",
+        "title" : "harry University",
+        "value" : 50000,
+        "id":"1"
+    },
+    {
+        "category" : "traveling",
+        "title" : "tommy University",
+        "value" : 50000,
+        "id":"2"
+    },
+    {
+        "category" : "education",
+        "title" : "jerry University",
+        "value" : 50000,
+        "id":"3"
+    },
+    { 
+        "category" : "business",
+        "title" : "Charlie University",
+        "value" : 50000,
+        "id":"4"
+    }
+]
+console.log(_.size(goal));
