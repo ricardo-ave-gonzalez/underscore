932c9a75669ac01d4944e00b6ab46eab24633e45
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2021-06-05 02:09:53 -0300

    training size in the project SICON

--- training.js
+++ training.js
@@ -1,40 +1,3 @@
-const { _ } = require('underscore')
-/**
- *      Return the number of values in the list.
- *      _.size([1, 2, 3, 4, 5]);
- *      => 5
- *      _.size({one: 1, two: 2, three: 3});
- *      => 3
- */
-
-let goal = [
-    {
-        "category": "other",
-        "title": "harry University",
-        "value": 50000,
-        "id": "1"
-    },
-    {
-        "category": "traveling",
-        "title": "tommy University",
-        "value": 50000,
-        "id": "2"
-    },
-    {
-        "category": "education",
-        "title": "jerry University",
-        "value": 50000,
-        "id": "3"
-    },
-    {
-        "category": "business",
-        "title": "Charlie University",
-        "value": 50000,
-        "id": "4"
-    }
-]
-console.log(_.size(goal));
-
 let contratistas = [
     {
         "id": "32e9ab05-a3a0-404d-9a4b-b9148b4e95f5",
@@ -574,23 +537,3 @@ console.log(_.size(id.personaVehiculo))
 //var grouped = _.groupBy(contratistas, 'length')
 //console.log(grouped)
 //console.log(_.sortBy(contratistas, 'persona'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
