78e9c89e88ba3087da482dc3b8893ff5985ceebe
ricardo-ave-gonzalez <ricardo.ave.gonzalez@gmail.com>
2022-02-02 09:19:28 -0300

    estructura de proyecto

---
+++ exercises/example_reduce.js
@@ -0,0 +1,22 @@
+const { _ } = require('underscore')
+
+let productArray = [
+    {
+        productId: 55,
+        productName: "Libero",
+        productQuantity: 7,
+        productPrice: 100
+    },
+    {
+        productId: 56,
+        productName: "Nam Libero Tempore",
+        productQuantity: 8,
+        productPrice: 150
+    }
+]
+
+const resultado = _.reduce(productArray, (accumulator, { productPrice, productQuantity }) => {
+    return accumulator + (productQuantity * productPrice)
+}, 0)
+
+console.log(resultado)
